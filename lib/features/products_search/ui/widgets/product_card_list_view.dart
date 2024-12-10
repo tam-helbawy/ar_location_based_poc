86f13f534f9c84e7d9e8ebfd8a5f71b0c427f180
helbawy <mohamed.elhelbawy.ac@gmail.com>
2024-12-10 19:24:42 +0200

    tool(qr_scanner): - build scanner page - import scanner plugin
    tool(file_opener):
    - import Open File plugin
    - build FileOener class

--- lib/features/products_search/ui/widgets/product_card_list_view.dart
+++ lib/features/products_search/ui/widgets/product_card_list_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../domain/models/product.dart';
 
@@ -10,6 +12,8 @@ class ProductCardListView extends StatelessWidget {
     return  ListTile(
       contentPadding: EdgeInsets.zero,
       isThreeLine: true,
+      onTap: (){
+      },
       leading: Image.network(product.imageUrl, width: 75, height: 75, fit: BoxFit.cover),
       title: Text(product.name),
       subtitle: Text(product.description),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import cloud_firestore
 import file_selector_macos
 import firebase_auth
 import firebase_core
+import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
