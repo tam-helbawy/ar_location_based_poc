c6bdc845a189ca62f403039fcacb0a9ae3714edf
Ahmad Alsehaim <a7madswe@gmail.com>
2025-01-07 14:01:29 +0300

    feat(ios): add changes

--- lib/features/location_director/domain/models/ar_geolocation_node.dart
+++ lib/features/location_director/domain/models/ar_geolocation_node.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 import 'dart:developer' as dev;
-import 'package:ar_flutter_plugin/models/ar_node.dart';
+import 'package:ar_flutter_plugin_flutterflow/models/ar_node.dart';
 import 'package:base/features/location_director/domain/models/map_location.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:vector_math/vector_math_64.dart' ;
@@ -49,4 +49,3 @@ class ARGeolocationNode extends ARNode {
   }
 
 }
-
